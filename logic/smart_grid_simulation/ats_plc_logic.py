c0e41d94d4bb9fd1f1d16a64bafd1ad0c0c765af
Jaxson Brown <jokesene@outlook.com>
2025-04-04 09:44:31 +0800

    organisation

--- logic/smart_grid_simulation/ats_plc_logic.py
+++ logic/smart_grid_simulation/ats_plc_logic.py
@@ -4,6 +4,8 @@ import time
 # the keys are defined in the JSON
 def logic(input_registers, output_registers, state_update_callbacks):
     state_change = True
+    sp_pm_prev = None
+    ts_prev = None
 
     while True:
         # get the solar panel power meter value reference
@@ -11,14 +13,25 @@ def logic(input_registers, output_registers, state_update_callbacks):
 
         # get the transfer switch output value reference
         ts_value = next((i for i in output_registers["coil"] if i["address"] == 200), None)
+        
+        if sp_pm_prev != sp_pm_value["value"]:
+            print(f"SOLAR PANEL POWER METER READING: {sp_pm_value['value']}")
+            sp_pm_prev = sp_pm_value["value"]
+        
+        if ts_prev != ts_value["value"]:
+            print(f"TRANSFER SWITCH VALUE: {ts_value['value']}")
+            ts_prev = ts_value["value"]
 
         # write to the transfer switch
+        # note that we retrieve the value by reference only (["value"])
         if sp_pm_value["value"] > 200 and state_change == True:
             ts_value["value"] = True
             state_change = False
             state_update_callbacks["transfer_switch_state"]()
+            print("TRANSFER SWITCH SWITCHED TO SOLAR POWER")
         if sp_pm_value["value"] <= 200 and state_change == False:
             ts_value["value"] = False
             state_change = True
             state_update_callbacks["transfer_switch_state"]()
+            print("TRANSFER SWITCH SWITCHED TO MAINS POWER")
         time.sleep(0.05)
--- logic/smart_grid_simulation/electrical_hil_logic.py
+++ logic/smart_grid_simulation/electrical_hil_logic.py
@@ -11,12 +11,13 @@ def logic(physical_values, interval):
 
     mean = 0
     std_dev = 1
-    height = 2000
+    height = 500
+    entries = 48
 
-    x_values = np.linspace(mean - 4*std_dev, mean + 4*std_dev, 100)
+    x_values = np.linspace(mean - 4*std_dev, mean + 4*std_dev, entries)
     y_values = height * np.exp(-0.5 * ((x_values - mean) / std_dev) ** 2)
 
-    solar_power_thread = Thread(target=solar_power_sim, args=(y_values, physical_values))
+    solar_power_thread = Thread(target=solar_power_sim, args=(y_values, physical_values, entries))
     solar_power_thread.daemon = True
     solar_power_thread.start()
 
@@ -27,9 +28,9 @@ def logic(physical_values, interval):
 
 def transfer_switch_sim(physical_values):
     while True:
-        print(physical_values["transfer_switch_state"])
-        print(physical_values["solar_power"])
-        print(physical_values["input_power"])
+        #print(physical_values["transfer_switch_state"])
+        #print(physical_values["solar_power"])
+        #print(physical_values["input_power"])
         if physical_values["transfer_switch_state"] == True:
             physical_values["input_power"] = physical_values["solar_power"]
         else:
@@ -37,10 +38,10 @@ def transfer_switch_sim(physical_values):
         time.sleep(0.1)
 
 
-def solar_power_sim(y_values, physical_values):
+def solar_power_sim(y_values, physical_values, entries):
     while True:
         # implement solar power simulation
-        for i in range(100):
+        for i in range(entries):
             solar_power = y_values[i]
             physical_values["solar_power"] = solar_power
             time.sleep(1)
